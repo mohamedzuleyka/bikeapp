9090d1df6c6787ae3d9bc5eb5c4911cb6e0fd582
zuleyka <zuleykam@Zuleykas-Surface.localdomain>
2026-01-08 13:32:09 -0600

    fixed vite base path for amplify deployment

--- microservices/bike-app/src/main.jsx
+++ microservices/bike-app/src/main.jsx
@@ -6,8 +6,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/absproxy/8081">
+    <BrowserRouter basename={import.meta.env.DEV ? "/absproxy/8081" : "/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
+
+
+// /basename="/absproxy/8081"
--- microservices/bike-app/vite.config.js
+++ microservices/bike-app/vite.config.js
@@ -2,15 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  // base: mode === 'production' ? '/absproxy/8081/' : '/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 8081,
     strictPort: true
-  },
-  base: '/absproxy/8081/',
-  test: {
-    environment: "jsdom",
-  },
-})
+    },
+    base: mode === 'development' ? '/absproxy/8081/' : '/',
+  }))
+
+
+
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//     port: 8081,
+//     strictPort: true
+//   },
+//   base: '/absproxy/8081/',
+//   test: {
+//     environment: "jsdom",
+//   },
+// })
